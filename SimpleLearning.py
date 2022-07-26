8e60d3ba9e72b5ada14620aeb4e624b3c3e92bcb
andriyashchenko <108915770+andriyashchenko@users.noreply.github.com>
2022-07-26 08:32:11 +0300

    Тренировка и прогнозирование

--- SimpleLearning.py
+++ SimpleLearning.py
@@ -1,5 +1,5 @@
 
-# y = 2x1 +3*x2 + 4*x3
+# y = 2*x1 + 3*x2 + 4*x3
 
 
 from random import randint
@@ -15,9 +15,21 @@ for i in range(train_set_count):
     a = randint(0, train_set_limit)
     b = randint(0, train_set_limit)
     c = randint(0, train_set_limit)
-    op = a + (2*b) + (3*c)
+    op = 2*a + (3*b) + (4*c)
     train_input.append([a, b, c])
     train_output.append(op)
 
 for i in range(20):
     print(train_input[i], train_output[i])
+    
+# Тренировка
+predictor = LinearRegression()
+predictor.fit(X=train_input, y=train_output)
+
+# Прогнозирование
+x_test = [[30, 40, 50]]
+outcome = predictor.predict(X=x_test)
+coefficients = predictor.coef_
+
+print('Outcome : ', outcome)
+print('Coefficients : ', coefficients)
