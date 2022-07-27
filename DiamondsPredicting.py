015842c8e88b91bc3f2656c0a29e1172f996d116
andriyashchenko <108915770+andriyashchenko@users.noreply.github.com>
2022-07-27 09:58:32 +0300

    Добавление файла DiamondsPredicting

---
+++ DiamondsPredicting.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as mb
+import seaborn as snb
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('P1_diamonds.csv')
+
+# ~ print(df.head(10).to_string())
+
+# Удаление Unnamed столбца, axis = 1 означает что мы удаляем столбец
+df = df.drop(['Unnamed: 0'], axis = 1)
+
+# Создание переменных для категорий
+categorical_features = ['cut', 'color', 'clarity']
+le = LabelEncoder()
+
+# Замена категорий на численные значения
+for i in range(3):
+    new = le.fit_transform(df[categorical_features[i]])
+    df[categorical_features[i]] = new
+    
+print(df.head(10).to_string())
